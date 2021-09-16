77df02d5460ba3598cd8efd8d0d72f15c7e530df
Yoon Sangho <yoonsangho@Yoons-MacBook-Pro.local>
2021-09-16 21:24:17 +0800

    edited

--- Planner.java
+++ Planner.java
@@ -1,3 +1,10 @@
+import java.io.FileWriter;
+import java.io.IOException;
+// import java.time.Duration;
+// import java.time.LocalDateTime;
+
+class AlreadyExistsException extends Exception {};
+class NotFoundException extends Exception {};
 
 public class Planner {
     String project;
@@ -12,25 +19,32 @@ public class Planner {
 
 
     public String toString() {
-        return  "Project: " + name ;
+        return  "Project: " + project ;
     }
+
+    void addBoard(IBoard b) throws AlreadyExistsException;
+    void addProject(IProject p) throws AlreadyExistsException; Iterable<IBoard> getBoards();
+    Iterable<IProject> getProjects();
+    public String writeXMLData();
+    /*
+        input: raw data from an XML file.
+    */
+    public void readXMLData(String data);
 
     // Function to convert project object to XML
     public String plannerToXML(){
         return "<?xml version = \"1.0\" encoding = \"UTF-8\" ?>" + "\n"
                 + "<planner>" + "\n"
-                + "    <name>" + getName() + "</name>" + "\n"
-                + "    <description>" + getDescription() + "</description>" + "\n"
-                + "    <deadline>" + getDeadline() + "</deadline>" + "\n"
+                + "    <project>" + getProject() + "</project>" + "\n"
                 + "</planner>";
     }
 
     public static void main(String[] args) throws IOException {
         //Sample Planner
-        Planner p1 = new Planner("Planner 1", "Planner 1 description", Duration.ofHours(5), LocalDateTime.of(2000, 8, 31, 7, 30));
+        Planner p1 = new Planner("Planner 1");
 
         // Turn Project object to xml file
-        String xmlStr = t1.plannerToXML();
+        String xmlStr = p1.plannerToXML();
         java.io.FileWriter fw = new FileWriter("planner.xml");
         fw.write(xmlStr);
         fw.close();
