a7bb4eacec474187df7e5735df251874863bb089
Hana Nabilah Fatinah <raihanahnabilah@gmail.com>
2021-09-16 23:03:10 +0800

    Remove schedulable and scheduledtask

--- Schedulable.java
+++
@@ -1,9 +0,0 @@
-package com.example.lib;
-
-import java.time.Duration;
-import java.time.LocalDateTime;
-
-public interface Schedulable {
-    void schedule(Duration estimatedDuration, LocalDateTime deadline);
-    void unschedule();
-}
